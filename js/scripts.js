966bdfb2eabfc522d32ad7e2141360e4e8210308
Majd Karoun <107414000+majd-karoun@users.noreply.github.com>
2023-03-10 13:22:59 +0100

    Pokemon list array made

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
-alert('Hello world');
 
-let favoriteFood = Pizz;
-document.write(favoriteFood);
+
+let pokemonList = [
+    {
+        name: 'Dragon',
+        height: 20,
+        type: ['ground', 'fire']
+    },
+    {
+        name: 'Tiger',
+        height: 4,
+        type: ['ground', 'rock']
+    },
+    {
+        name: 'Shark',
+        height: 7,
+        type: ['water', 'blaze']
+    }
+];
+
+console.log(pokemonList)
