c039b710df266db41f9007016948adc8dedbeb7b
Majd Karoun <107414000+majd-karoun@users.noreply.github.com>
2023-03-14 10:36:30 +0100

    for loop to render list made

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,39 @@
-
+let body = $('.body'
+)
 
 let pokemonList = [
-    {
-        name: 'Dragon',
-        height: 20,
-        type: ['ground', 'fire']
-    },
-    {
-        name: 'Tiger',
-        height: 4,
-        type: ['ground', 'rock']
-    },
-    {
-        name: 'Shark',
-        height: 7,
-        type: ['water', 'blaze']
-    }
+  {
+    name: "Dragon",
+    height: 20,
+    type: ["ground", "fire"],
+  },
+  {
+    name: "Tiger",
+    height: 4,
+    type: ["ground", "rock"],
+  },
+  {
+    name: "Shark",
+    height: 7,
+    type: ["water", "blaze"],
+  },
 ];
 
-console.log(pokemonList)
+// loop over Pokemon List
+for (let i = 0; i < pokemonList.length; i++) {
+  // check if pokemon is Tiger
+  let pokemon = pokemonList[i].name
+  if (pokemon.name=== "Dragon") {
+    document.write(
+      `${pokemon.name} (height ${
+        pokemon.name
+      }) ${"Wow, that's fast!"}`
+    );
+  } else {
+    document.write(`${pokemonList[i].name} (height ${pokemonList[i].height})`);
+  }
+  let typesElement = $(
+    '<p>' + '<strong>' + 'Type/s: ' + '</strong>' + '</p>'
+  )
+ 
+}
