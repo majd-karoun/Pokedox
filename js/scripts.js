f1dd631452bf796f9b5e8c8cf62e74ccaf07c09e
Majd Karoun <107414000+majd-karoun@users.noreply.github.com>
2023-03-15 11:01:41 +0100

    added the pokemonRepository IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,42 @@
-let pokemonList = [
-  {
-    name: "Dragon",
-    height: 20,
-    type: ["ground", "fire"],
-  },
-  {
-    name: "Tiger",
-    height: 4,
-    type: ["ground", "rock"],
-  },
-  {
-    name: "Shark",
-    height: 7,
-    type: ["water", "blaze"],
-  },
-];
 
 
+let pokemonRepository = (function(){
+  let pokemonList = [
+    {
+      name: "Dragon",
+      height: 20,
+      type: ["ground", "fire"],
+    },
+    {
+      name: "Tiger",
+      height: 4,
+      type: ["ground", "rock"],
+    },
+    {
+      name: "Shark",
+      height: 7,
+      type: ["water", "blaze"],
+    },
+  ];
 
-pokemonList.forEach(item => {
+  function getAll(){
+    return pokemonList
+  }
+
+  function add(item){
+    return pokemonList.push(item)
+  }
+
+  return{
+  
+    getAll,
+    add
+  }
+
+}())
+
+
+pokemonRepository.getAll().forEach(item => {
   let pokemon = item.name;
   let pokemonHeight = item.height;
    // check if pokemon is Dragon
